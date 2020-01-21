3f84a724a70ae752362b8bb1c265e1ffbc61035e
iyasmzn <iyasmzn07@gmail.com>
2020-01-21 11:24:11 +0700

    try sticy navbar, but it didn't work

---
+++ css_responsive_ust_alfin/assets/script.js
@@ -0,0 +1,12 @@
+// window.onscroll = function() {sticky_navbar()};
+// var nav = document.getElementById('navbar');
+// var sticky = nav.offsetTop();
+
+// function sticky_navbar() {
+// 	if (window.pageYOffset > sticky ) {
+// 		nav.classlist.add('sticky');
+// 	}
+// 	else {
+// 		nav.classlist.remove('sticky');
+// 	}
+// }
